d264e414fad9af344abb16735cae53ea64c1f39a
Dong Bin <popmessiah@hotmail.com>
2025-02-06 12:26:22 +0800

    feat: add tree event demo.

---
+++ Tutorials/Tree_Expand_Event/MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Tree_Expand_Event;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TreeViewItem_OnExpanded(object? sender, RoutedEventArgs e)
+    {
+        if (e.Source is TreeViewItem item)
+        {
+            text.Text = item.Header?.ToString();
+        }
+    }
+}
